4eead3fbd5315b7ec04a4855bde26838ebf63435
Yeshu Lokesh <yeshu1108@gmail.com>
2023-10-15 20:33:19 -0400

    Updated skeleton code

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-''' Main server file! '''
+from flask import Flask, make_response, render_template
+import bcrypt
+import hashlib
+import random
+
+app = Flask(__name__,template_folder='template')
+
+@app.route("/")
+def indexPage():
+    resp = make_response(render_template('index.html'))
+    return resp
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8080,debug=True)
+
+

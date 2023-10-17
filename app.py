a6bbc61b70b4fc518198965cf99e3b63af09e52c
r14dd <riadmukh@buffalo.edu>
2023-10-16 20:15:57 -0400

    updated the login code

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import secrets
+
 from flask import Flask, make_response, render_template, request, send_from_directory
 import bcrypt
 import hashlib
@@ -7,13 +9,14 @@ from pymongo import MongoClient
 mongo_client = MongoClient("mongo")
 db = mongo_client["cse312"]
 user_collection = db["users"]
+token_collection = db["tokens"]
 
 app = Flask(__name__,template_folder='template')
 
 @app.route("/")
 def indexPage():
-    resp = make_response(render_template('index.html'))
-    return resp
+    usr = request.cookies.get('username')
+    return render_template('index.html', usr=usr)
 
 @app.route("/static/style.css")
 def css():
@@ -27,8 +30,26 @@ def register():
         inputPassword = request.form['password']
         salt = bcrypt.gensalt()
         pwHash = bcrypt.hashpw(inputPassword.encode('utf-8'), salt)
-        user_collection.insert_one({"username": inputUsername, "password": pwHash.decode('utf-8')})
-        return "Account Registered"
+        user_collection.insert_one({"username": inputUsername, "password": pwHash})
+        return make_response("U on da boat now!", 200)
+
+@app.route("/login", methods=['POST'])
+def login():
+    inputUsername = request.form['username']
+    inputPassword = request.form['password']
+    user = user_collection.find_one({'username' : inputUsername})
+
+    if user:
+        if bcrypt.checkpw(inputPassword.encode('utf-8'), user['password']):
+            resp = make_response("Now ur logged in", 200)
+            auth_token = secrets.token_urlsafe(30)
+            hashed_auth_token = str(hashlib.sha256(auth_token))
+            token_collection.insert_one({"auth_token" : hashed_auth_token, "username" : user["username"]})
+            resp.set_cookie('auth_token', hashed_auth_token, max_age=3600)
+            return resp
+        return make_response("Invalid username or password", 401)
+    else:
+        return make_response("You have to register first!", 401)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080,debug=True)
